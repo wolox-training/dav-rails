9a1fef506e7b32bd8b7f7e3a043be4a4b5ba63a5
Daniel Vega <deveh91@gmail.com>
2021-06-02 09:49:22 -0500

    add contant and .slice to service find_book

--- app/services/books_service.rb
+++ app/services/books_service.rb
@@ -3,10 +3,11 @@ class BooksService
   base_uri Rails.application.credentials.base_uri
 
   def find_book(id)
+    path = '/api/books'
     options = { query: { bibkeys: id, format: 'json', jscmd: 'data' } }
-    book = self.class.get('/api/books', options)
+    book = self.class.get(path, options)
 
-    keys = %w[title subtitle number_of_pages authors]
-    JSON.parse(book.body)[id].keep_if { |k, _| keys.include?(k) }
+    keys = [:title, :subtitle, :number_of_pages, :authors]
+    JSON.parse(book.body)[id].deep_symbolize_keys.slice(*keys)
   end
 end
