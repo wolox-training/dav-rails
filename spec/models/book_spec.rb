ee9ac79209d7953c7165d1d2e9220248f9511cbb
Daniel Vega <deveh91@gmail.com>
2021-06-08 17:45:33 -0500

    create tests for book model

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Book do
+  context 'with valid model' do
+    subject(:book) { build(:book) }
+    it { is_expected.to be_valid }
+    it { is_expected.to validate_presence_of(:genre) }
+    it { is_expected.to validate_presence_of(:author) }
+    it { is_expected.to validate_presence_of(:image) }
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:editor) }
+    it { is_expected.to validate_presence_of(:year) }
+  end
+
+  context 'with invalid model' do
+    subject(:book) { build(:book, genre: nil) }
+    it { is_expected.not_to be_valid }
+  end
 end
