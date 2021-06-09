de39e551a2305a9c37b17aaca2c6e1f87fd9e4f5
Daniel Vega <deveh91@gmail.com>
2021-06-09 17:59:35 -0500

    move filter to book model

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,13 +2,8 @@ class BooksController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    books = Rack::Reducer.call(params, dataset: Book.all, filters: [
-      ->(author:) { where('lower(author) like ?', "%#{author.downcase}%") },
-      ->(genre:) {  where('lower(genre) like ?', "%#{genre.downcase}%") },
-      ->(title:) {  where('lower(title) like ?', "%#{title.downcase}%") }
-      ])
-    
-      render_paginated books, each_serializer: BookSerializer
+    books = Book::Reducer.apply(params)
+    render_paginated books, each_serializer: BookSerializer
   end
 
   def show
