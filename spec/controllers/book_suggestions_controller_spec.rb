24815fd40614b4888f5a9044a257fcfcdddbc886
Daniel Vega <deveh91@gmail.com>
2021-06-11 09:40:33 -0500

    change book_suggestion_controller tests

--- spec/controllers/book_suggestions_controller_spec.rb
+++ spec/controllers/book_suggestions_controller_spec.rb
@@ -4,6 +4,10 @@ describe BookSuggestionsController, type: :controller do
   describe 'POST #create' do
     context 'When creating a valid book suggestion' do
       let!(:book_suggestion) { attributes_for(:book_suggestion) }
+      before do
+        post :create, params: { book_suggestion: book_suggestion }
+      end
+      
       it 'creates a new rent' do
         expect do
           post :create, params: { book_suggestion: book_suggestion }
@@ -11,7 +15,6 @@ describe BookSuggestionsController, type: :controller do
       end
 
       it 'responds with 201 status' do
-        post :create, params: { book_suggestion: book_suggestion }
         expect(response).to have_http_status(:created)
       end
     end
@@ -29,10 +32,10 @@ describe BookSuggestionsController, type: :controller do
       end
 
       it 'returns error messages' do
-        expect(response.body['Error']).to be_present
+        expect(response.body['error']).to be_present
       end
 
-      it 'responds with 422 status' do
+      it 'responds with 400 status' do
         expect(response).to have_http_status(:bad_request)
       end
     end
