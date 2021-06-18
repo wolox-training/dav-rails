4c24c7f27c3ff9628a27655317d6466db206f113
Daniel Vega <deveh91@gmail.com>
2021-06-18 16:45:18 -0500

    Add books seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+1000.times do
+  Book.create(
+    genre: Faker::Book.genre,
+    author: Faker::Book.author,
+    image: Faker::Internet.url(host: 'wolox.com.ar', path: '/image.jpg'),
+    title: Faker::Book.title.truncate(25),
+    editor: Faker::Book.publisher,
+    year: Faker::Number.between(from: 1000, to: 2019),
+    times_rented: Faker::Number.between(from: 1, to: 20)
+  )
+end
